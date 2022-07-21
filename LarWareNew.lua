472b004daadcd6b294f1c4bd1a84b63b165e1a02
Larwack <kenneethlee@gmail.com>
2022-07-21 12:57:46 -0700

    new ui

--- LarWareNew.lua
+++ LarWareNew.lua
@@ -1,16 +1,48 @@
-local UILib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local GameId = game.GameId
 
-local function Create()
-    local GameName = game:GetService("MarketplaceService"):GetProductInfo(PLACEID).Name
-end
+local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/ZepsyyCodesLUA/Ellik/main/source.lua', true))()
 
 local SupportedGames = {
-    [5872075530] = function()
+    [5872075530] = function(Window) -- anarchy
+        local TriggerBot = false
+        local AimAssist = false
+
+        local CombatTab = Window:NewTab({
+            Title = "Combat"
+        })
+
+        CombatTab:Toggle({
+            Text = "Trigger Bot",
+            Callback = function(boolean)
+                TriggerBot = boolean
+            end
+        })
+    end,
+
+    [2262441883] = function(Window)
         
     end,
-    [2262441883] = function()
+
+    [3297964905] = function(Window)
         
     end,
-    
+
 }
 
+local function CreateUI()
+    for Id, Function in pairs(SupportedGames) do
+        if Id == GameId then
+            local GameName = game:GetService("MarketplaceService"):GetProductInfo(GameId).Name
+
+            local Window = UILib:Create({
+                Title = "LarWare "..GameName
+            })
+
+            local WelcomeTab = Window:NewTab({
+                Title = "Welcome!"
+            })
+
+            Function(Window)
+        end
+    end
+end
--- LarWare_Testing.lua
+++ LarWare_Testing.lua
@@ -1,72 +1,48 @@
+local GameId = game.GameId
+
+local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/ZepsyyCodesLUA/Ellik/main/source.lua', true))()
+
 local SupportedGames = {
-    5872075530,
-    2262441883,
-    
+    [5872075530] = function(Window) -- anarchy
+        local TriggerBot = false
+        local AimAssist = false
+
+        local CombatTab = Window:NewTab({
+            Title = "Combat"
+        })
+
+        CombatTab:Toggle({
+            Text = "Trigger Bot",
+            Callback = function(boolean)
+                TriggerBot = boolean
+            end
+        })
+    end,
+
+    [2262441883] = function(Window)
+        
+    end,
+
+    [3297964905] = function(Window)
+        
+    end,
+
 }
 
-local UILib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local function CreateUI()
+    for Id, Function in pairs(SupportedGames) do
+        if Id == GameId then
+            local GameName = game:GetService("MarketplaceService"):GetProductInfo(GameId).Name
 
-local Window = UILib.CreateLib("LarWare", "Serpent")
+            local Window = UILib:Create({
+                Title = "LarWare "..GameName
+            })
 
--- Tab Creation
-local Tab_Welcome = Window:NewTab("Welcome!")
+            local WelcomeTab = Window:NewTab({
+                Title = "Welcome!"
+            })
 
-local Tab_Player = Window:NewTab("Player")
-local Tab_Combat = Window:NewTab("Combat")
-local Tab_Environment = Window:NewTab("Environment")
-local Tab_Fun = Window:NewTab("Fun")
-
---Player Section
-local Player_Speed = Tab_Player:NewSection("Speed")
-local Player_JumpPower = Tab_Player:NewSection("Jump Power")
-local Player_SemiGod = Tab_Player:NewSection("Semi-God")
-
---Combat Section
-local Combat_Aimbot = Tab_Combat:NewSection("Aimbot")
-local Combat_AntiAim = Tab_Combat:NewSection("Anti-Aim")
-
---Environment Section
-local Environment_Weather = Tab_Environment:NewSection("Weather")
-
---Fun Section
-
---Welcome Label
-local Tab_Welcome = Tab_Player:NewSection("WAWHDAWD")
-
---Player Label
-
---Combat Label
-
---Environment Label
-
---Fun Label
-
-local Player_SpeedValue = false
-local Player_SemiGodValue = false
-local Player_JumpPowerValue = false
-
---Toggles
-Player_Speed:NewToggle("On/Off", "ToggleInfo", function(state)
-    Player_SpeedValue = state
-end)
-
-Player_JumpPower:NewToggle("On/Off", "Jump Power", function(state)
-    Player_JumpPowerValue = state
-end)
-
-Player_SemiGod:NewToggle("On/Off", "Semi-God", function(state)
-    Player_SemiGodValue = state
-end)
-
---Sliders
-Player_Speed:NewSlider("Speed", "Speed", 100, 0, function(Value)
-    if Player_SpeedValue then
-
+            Function(Window)
+        end
     end
-end)
-
-Player_JumpPower:NewSlider("Jump Power", "Jump", 300, 50, function(Value)
-    if Player_JumpPowerValue then
-
-    end
-end)
+end
