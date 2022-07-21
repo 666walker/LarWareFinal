9e02c5fda230d9a5fd786ba63a29661067284116
Larwack <kenneethlee@gmail.com>
2022-07-21 11:26:54 -0700

    new script

---
+++ LarWareNew.lua
@@ -0,0 +1,16 @@
+local UILib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local function Create()
+    local GameName = game:GetService("MarketplaceService"):GetProductInfo(PLACEID).Name
+end
+
+local SupportedGames = {
+    [5872075530] = function()
+        
+    end,
+    [2262441883] = function()
+        
+    end,
+    
+}
+
